44bf3b56eccf84ed7224c32d1929d7e7e447cef5
meahesachin <meahesachin@gmail.com>
2020-10-26 19:41:41 -0400

    Feature: Removed duplication by using setUp method

--- src/calculatorTests.py
+++ src/calculatorTests.py
@@ -4,42 +4,36 @@ from calculator import Calculator
 
 class MyTestCase(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
     def test_instantiate_calculator(self):
-        calculator = Calculator()
-        self.assertIsInstance(calculator, Calculator)
+        self.assertIsInstance(self.calculator, Calculator)
 
     def test_addition(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.add(1,1),2)
+        self.assertEqual(self.calculator.add(1, 1), 2)
 
     def test_subtraction(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.subtract(1,1),0)
+        self.assertEqual(self.calculator.subtract(1, 1), 0)
 
     def test_results_property(self):
-        calculator = Calculator()
-        calculator.add(2, 1)
-        self.assertEqual(calculator.result, 3)
+        self.calculator.add(2, 1)
+        self.assertEqual(self.calculator.result, 3)
 
     def test_multiply(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.multiply(2, 3), 6)
+        self.assertEqual(self.calculator.multiply(2, 3), 6)
 
     def test_divide(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.divide(6, 3), 2)
+        self.assertEqual(self.calculator.divide(6, 3), 2)
 
     def test_dividebyzero(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.divide(6, 0), 0)
+        self.assertEqual(self.calculator.divide(6, 0), 0)
 
     def test_square(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.square(2), 4)
+        self.assertEqual(self.calculator.square(2), 4)
 
     def test_sqrt(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.sqrt(4), 2)
+        self.assertEqual(self.calculator.sqrt(4), 2)
 
 
 if __name__ == '__main__':
