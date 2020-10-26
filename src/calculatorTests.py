02a08a80a851ec0b9d0a5e102d4a41de8dfef93c
meahesachin <meahesachin@gmail.com>
2020-10-26 17:36:15 -0400

    Feature: Updated code in calculatorTests.py to resolve errors due to bad typing.

--- src/calculatorTests.py
+++ src/calculatorTests.py
@@ -9,16 +9,17 @@ class MyTestCase(unittest.TestCase):
 
     def test_additon(self):
         calculator = Calculator()
-        self.assertEq(calculator.add(1,1),2)
+        self.assertEqual(calculator.add(1,1),2)
 
     def test_subtraction(self):
         calculator = Calculator()
-        self.assertEq(calculator.subtract(1,1),0)
+        self.assertEqual(calculator.subtract(1,1),0)
 
     def test_results_property(self):
         calculator = Calculator()
         calculator.add(2, 1)
         self.assertEqual(calculator.result, 3)
+
 
 if __name__ == '__main__':
     unittest.main()
